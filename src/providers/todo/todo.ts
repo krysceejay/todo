7be4ea2f59a7fcadb583cb7ef1d494e8c42e8dd9
Chris <chrischimachijioke@gmail.com>
2018-01-24 09:16:50 +0100

    second commit

---
+++ src/providers/todo/todo.ts
@@ -0,0 +1,40 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the TodoProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TodoProvider {
+  private todos = [];
+  private archivedTodos = [];
+
+  constructor(public http: Http) {
+    console.log('Hello TodoProvider Provider');
+  }
+
+  archiveTodo(todoIndex){
+    let todoToBeArchived = this.todos[todoIndex];
+    this.todos.splice(todoIndex, 1);
+    this.archivedTodos.push(todoToBeArchived);
+  }
+
+  getTodos(){
+    return this.todos;
+  }
+
+  getArchivedTodos(){
+    return this.archivedTodos;
+  }
+
+  addTodo(todo){
+    this.todos.push(todo);
+  }
+
+  editTodo(todo, todoIndex){
+    this.todos[todoIndex] = todo;
+  }
+}
